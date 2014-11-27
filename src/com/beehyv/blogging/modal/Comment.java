6ddecc22a86ef1259625e54ef76d1969b840db3b
rgrg27 <rgrg27@gmail.com>
2014-11-27 15:29:42 +0530

    test

--- src/com/beehyv/blogging/modal/Comment.java
+++ src/com/beehyv/blogging/modal/Comment.java
@@ -10,73 +10,73 @@ public class Comment {
 	/**
 	 * @return the comment
 	 */
-	private String getComment() {
+	public String getComment() {
 		return comment;
 	}
 	/**
 	 * @param comment the comment to set
 	 */
-	private void setComment(String comment) {
+	public void setComment(String comment) {
 		this.comment = comment;
 	}
 	/**
 	 * @return the created_at
 	 */
-	private String getCreated_at() {
+	public String getCreated_at() {
 		return created_at;
 	}
 	/**
 	 * @param created_at the created_at to set
 	 */
-	private void setCreated_at(String created_at) {
+	public void setCreated_at(String created_at) {
 		this.created_at = created_at;
 	}
 	/**
 	 * @return the updated_at
 	 */
-	private String getUpdated_at() {
+	public String getUpdated_at() {
 		return updated_at;
 	}
 	/**
 	 * @param updated_at the updated_at to set
 	 */
-	private void setUpdated_at(String updated_at) {
+	public void setUpdated_at(String updated_at) {
 		this.updated_at = updated_at;
 	}
 	/**
 	 * @return the idPost
 	 */
-	private long getIdPost() {
+	public long getIdPost() {
 		return idPost;
 	}
 	/**
 	 * @param idPost the idPost to set
 	 */
-	private void setIdPost(long idPost) {
+	public void setIdPost(long idPost) {
 		this.idPost = idPost;
 	}
 	/**
 	 * @return the idEmployee
 	 */
-	private long getIdEmployee() {
+	public long getIdEmployee() {
 		return idEmployee;
 	}
 	/**
 	 * @param idEmployee the idEmployee to set
 	 */
-	private void setIdEmployee(long idEmployee) {
+	public void setIdEmployee(long idEmployee) {
 		this.idEmployee = idEmployee;
 	}
 	/**
 	 * @return the name
 	 */
-	private String getName() {
+	public String getName() {
 		return name;
 	}
 	/**
 	 * @param name the name to set
 	 */
-	private void setName(String name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 }
