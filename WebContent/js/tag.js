1e85db5911f84cd0d3813e4761f288c4b0f8f18e
pooja <sripooja09@gmail.com>
2014-12-10 17:37:44 +0530

    tag js file

---
+++ WebContent/js/tag.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+function updateTags(){
+	
+
+	jQuery.ajax({
+		url: "tags?actionName=homeTags",
+		method: "GET",
+		contentType: "",
+		success:function(tags){
+			
+			var tagsArray = JSON.parse(tags)
+			
+			var $tagContainer = $(".tags");
+			for(var i in tagsArray){
+				//alert(tagsArray[i].idTag);
+				$tagContainer.append('<div><a href="tags/tag?actionName=postsbyTag&tagId='+tagsArray[i].idTag+'">'+tagsArray[i].tag+'</a></div>')
+				alert(i)
+			}
+			
+		},
+		error:function(e){
+			
+		}
+	});
+}
